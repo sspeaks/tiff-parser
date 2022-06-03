de84755ee47c52167706f607d6d02dec2d0ed6b1
Seth Speaks <sspeaks610@gmail.com>
2022-06-03 13:50:58 -0700

    add flake

--- flake.nix
+++ flake.nix
@@ -3,10 +3,9 @@
   inputs.flake-utils.url = github:numtide/flake-utils;
 
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      {
-        packages = (import ./default.nix)
-            { pkgs = nixpkgs.legacyPackages.${system}; };
-      }
-    );
+  flake-utils.lib.eachDefaultSystem (system:
+  {
+    packages = (import ./default.nix) {pkgs = nixpkgs.legacyPackages.${system};};
+    defaultPackage = self.packages.${system}.tiffParser;
+  });
 }
